1852b79b8842536adad4da643435c583e98e2b2e
Mikhail Komarov <komarov.ma@phystech.edu>
2026-08-08 11:38:48 +0200

    Update telescope config

--- config/lua/plugins/telescope.lua
+++ config/lua/plugins/telescope.lua
@@ -1,58 +1,89 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local action_state = require("telescope.actions.state")
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "master",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
 
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate" },
-        layout_strategy = "flex",
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-q>"] = function(prompt_bufnr)
-              local picker = action_state.get_current_picker(prompt_bufnr)
-              local prompt = nil
-              if _G.grep_word_buf then
-                prompt = _G.grep_word_buf
-              else
-                if picker._get_prompt then
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local action_state = require("telescope.actions.state")
+
+      telescope.setup({
+        defaults = {
+          path_display = {
+            "truncate",
+          },
+
+          file_ignore_patterns = {
+            "%.git/",
+          },
+
+          preview = {
+            treesitter = false,
+          },
+
+          layout_strategy = "flex",
+
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
+
+              ["<C-q>"] = function(prompt_bufnr)
+                local picker = action_state.get_current_picker(prompt_bufnr)
+
+                local prompt
+
+                if _G.grep_word_buf then
+                  prompt = _G.grep_word_buf
+                elseif picker and picker._get_prompt then
                   prompt = picker:_get_prompt()
                 end
-              end
-              if prompt then
-                _G.qf_search_term = prompt
+
+                if prompt then
+                  _G.qf_search_term = prompt
+                end
+
                 actions.send_to_qflist(prompt_bufnr)
                 actions.open_qflist()
-              end
-            end
+              end,
+            },
           },
         },
-      },
-      pickers = {
-        find_files = {
-          hidden = true,
+
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
         },
-      },
-    })
+      })
 
-    -- Load fzf extension for better performance
-    telescope.load_extension("fzf")
+      telescope.load_extension("fzf")
 
-    -- Keymaps
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
-  end,
+      local builtin = require("telescope.builtin")
+
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+        desc = "Find files",
+      })
+
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+        desc = "Live grep",
+      })
+
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+        desc = "Buffers",
+      })
+
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+        desc = "Help tags",
+      })
+    end,
+  },
 }
-
