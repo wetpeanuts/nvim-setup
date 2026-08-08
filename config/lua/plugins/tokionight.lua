45e5156a11eb6c6bccc61484c1aed1ab1049340e
Mikhail Komarov <komarov.ma@phystech.edu>
2026-08-08 11:39:14 +0200

    Changed main colorscheme to tokyonight-moon

--- config/lua/plugins/tokionight.lua
+++ config/lua/plugins/tokionight.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
 
     opts = {
-      style = "night", -- storm | moon | night | day
+      style = "moon", -- storm | moon | night | day
       transparent = false,
       terminal_colors = true,
 
@@ -19,7 +19,7 @@ return {
 
     config = function(_, opts)
       require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight-night")
+      vim.cmd.colorscheme("tokyonight-moon")
     end,
   },
 }
